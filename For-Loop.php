714ac1d3945845ebeaf07c19cc0b782bafb11c4c
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-10 23:16:47 +0500

    For Loop

--- For-Loop.php
+++ For-Loop.php
@@ -1,8 +1,7 @@
 <?php
 
 $box = 5;
-for($wrap = 0, $wrap <= $wrap box , $wrap++){
-    echo "Combination Of While And doWhile Loop"
+for($wrap = 0; $wrap <= $box; $wrap++;){
+    echo "Combination Of While And doWhile Loop";
 }
-
 ?>
