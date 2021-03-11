1b8e819002881620abb790fe14a6a9224b04b736
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-12 00:12:36 +0500

    Calculator in Php

--- Calculator.php
+++ Calculator.php
@@ -7,6 +7,14 @@
     <title>Php Calcualtor</title>
 </head>
 <body>
-    
+    <input type="text" name="num1">
+    <input type="text" name="num2">
+    <select name="operation" id="">
+        <option value="add">+</option>
+        <option value="sub">-</option>
+        <option value="mul">x</option>
+        <option value="div">/</option>
+    </select>
+
 </body>
 </html>
