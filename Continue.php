88782b74c074a8e0ab81bb18197e4dd85317f322
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-11 23:23:56 +0500

    Continue in Php

--- Continue.php
+++ Continue.php
@@ -1,10 +1,11 @@
 <?php
 for($x=1;$x<=10;$x++){
-    if($x==5){
-        echo "Ignore 5". $x ."</br>";
+    if($x == 5){
+        // echo "Ignore 5". $x ."</br>";
+        // break;
         continue;
     }
-    echo $x . "Continue Statement"."</br>";
+    echo $x . " ===> ". "Continue Statement"."</br>";
 
 }
 ?>
