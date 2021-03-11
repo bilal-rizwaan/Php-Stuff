03d661a731630cc17af2ae908215066bb903ef50
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-11 23:35:54 +0500

    Goto in Php

--- Goto.php
+++ Goto.php
@@ -1,11 +1,5 @@
 <?php
-for($x=1;$x<=10;$x++){
-    if($x == 5){
-        // echo "Ignore 5". $x ."</br>";
-        // break;
-        continue;
-    }
-    echo $x . " ===> ". "Continue Statement"."</br>";
+ $space = "Goto Method in Php";
+ echo $space;
 
-}
 ?>
