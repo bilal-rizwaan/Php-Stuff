e933f2e84dbbbf0a2aff5b0c03ce675d6d33e1a1
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-12 00:18:00 +0500

    Calculator in Php

--- Calculator.php
+++ Calculator.php
@@ -7,14 +7,28 @@
     <title>Php Calcualtor</title>
 </head>
 <body>
-    <input type="text" name="num1">
-    <input type="text" name="num2">
-    <select name="operation" id="">
-        <option value="add">+</option>
-        <option value="sub">-</option>
-        <option value="mul">x</option>
-        <option value="div">/</option>
-    </select>
+        <form method="POST">
+                <input type="text" name="num1">
+                <input type="text" name="num2">
+                <select name="operation" id="">
+                    <option value="add">+</option>
+                    <option value="sub">-</option>
+                    <option value="mul">x</option>
+                    <option value="div">/</option>
+                </select>
+                <input type="button" name="sub" value="Go">
+    </form>
+    <h1>
+    <?php
+    if(isset($_POST['sub'])){
+        $num1=$_POST['$num1'];
+        $num2=$_POST['$num2'];
+
+
+    }
+    
+    ?>
+    </h1>
 
 </body>
 </html>
