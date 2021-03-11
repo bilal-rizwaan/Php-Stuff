a63dc19e4b75e5573bc5359e8154433f23af188a
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-11 23:46:39 +0500

    Goto in Php

--- Goto.php
+++ Goto.php
@@ -1,5 +1,8 @@
 <?php
+ $space = "Goto Method in Php Version";
+ $space = "Goto Method in Php 8 ";
+ goto remove;
  $space = "Goto Method in Php";
- echo $space;
+ remove: echo "Remove the Statement";
 
 ?>
