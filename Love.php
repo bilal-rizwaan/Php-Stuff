72a2605e74df29acec6d932d3670a30597088a73
Bilal <bilal.rizwaan1909@gmail.com>
2021-03-23 00:41:43 +0500

    Love Calculator in Php [Complet]

---
+++ Love.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <form method="post">
+            <input type="text" name="love">
+            <input type="text" name="bird">
+            <input type="submit" name="find">
+        </form>
+
+        <?php
+        if(isset($_POST['submit'])){
+            $love = $_POST['love'];
+            $bird = $_POST['bird'];
+            $ran=rand();
+        echo $love + $bird '=' $rand;
+        }
+        ?>
+</body>
+</html>
